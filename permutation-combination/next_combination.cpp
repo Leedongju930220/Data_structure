586664c068a7788db56ee3647c62dddd03789f82
Lee dong ju <30771935+Leedongju930220@users.noreply.github.com>
2018-06-27 16:53:28 +0900

    Create next_combination.cpp

---
+++ permutation-combination/next_combination.cpp
@@ -0,0 +1,23 @@
+#include<iostream>  
+#include<vector>  
+#include<algorithm>  
+using namespace std;  
+  
+int main(){  
+      
+    vector<int> num;  
+  
+    for(int i=0 ; i<3 ; i++){  
+        num.push_back(i);  
+    }  
+  
+    do{  
+        for(int i=0 ; i<(int)num.size() ; i++) cout<<num[i]<<" ";  
+        cout<<endl;  
+    }while(next_permutation(num.begin(),num.end()));  
+  
+    return 0;  
+}  
+
+
+출처: http://alhena.tistory.com/43 [ALHENA'S WORLD]
