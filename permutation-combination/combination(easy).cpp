b7542173ef2c19ad103916542e9d5436569b6d58
Lee dong ju <30771935+Leedongju930220@users.noreply.github.com>
2018-08-17 00:00:33 +0900

    Update combination(easy).cpp

--- permutation-combination/combination(easy).cpp
+++ permutation-combination/combination(easy).cpp
@@ -1,17 +1,22 @@
 #include<cstdio>
-
-int num[9];
-int N = 8;
+#define MAX 6
+// N combination N/2
+int num[MAX];
+int N = MAX;
 // 8 conbination 4
 void combi(int p, int d){
-	if (d == 4){
-		for (int i = 1; i <= 8; i++){
+	if (d == MAX/2){
+		for (int i = 1; i <= MAX; i++){
 			if (num[i] == 1)printf("%d", i);
 		}
 		printf("\n");
+		for (int i = 1; i <= MAX; i++){
+			if (num[i] == 0) printf("%d", i);
+		}
+		printf("\n\n\n\n");
 		return;
 	}
-	if (p == 9) return;
+	if (p == MAX+1) return;
 	num[p] = 1;
 	combi(p + 1, d + 1);
 	num[p] = 0;
